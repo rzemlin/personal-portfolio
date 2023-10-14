9b48cd929e0e47e1e3790248478388672dc7ddc9
Ryan Zemlin <rzemlin@gmail.com>
2023-10-14 15:09:00 -0700

    modify Header w/links

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,42 @@
 import React from 'react'
 
 function Header() {
+    const menu = [ 
+        {
+            id: 1,
+            name: 'HOME',
+        },
+        {
+            id: 2,
+            name: 'SKILLS',
+        },
+        {
+            id: 3,
+            name: 'PROJECTS',
+        },
+        {
+            id: 4,
+            name: 'CONTACT',
+        },
+    ];
+    
   return (
     <div>
         <div className='w-[90px] h-[90px] bg-black '>
-            <img src= '/public/nautical compass.png'
+            <img src= '/public/compass.png'
             className='p-7'
             />
         </div>
+        <div>
+            {menu.map((item)=> (
+                // eslint-disable-next-line react/jsx-key
+                <div className='cursor-pointer'>
+                  {item.name}
+                </div>
+            ))}
+        </div>
     </div>
   )
-}
+};
 
 export default Header
