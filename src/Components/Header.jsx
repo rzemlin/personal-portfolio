ad9c2f38f1cf9088d80121cd2d36b19a53500f90
Ryan Zemlin <rzemlin@gmail.com>
2023-10-14 13:45:29 -0700

    add google font

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <div className='w-[90px] h-[90px] bg-black '>
+            Sup Mo Fucka
+        </div>
+    </div>
+  )
+}
+
+export default Header
