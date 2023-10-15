ca8662249aad6ddaa5af1fc6b5685349207e2c1b
Ryan Zemlin <rzemlin@gmail.com>
2023-10-15 10:24:36 -0700

    add css to header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Header() {
-    const menu = [ 
+    const menu = [
         {
             id: 1,
             name: 'HOME',
@@ -19,24 +19,31 @@ function Header() {
             name: 'CONTACT',
         },
     ];
-    
-  return (
-    <div>
-        <div className='w-[90px] h-[90px] bg-black '>
-            <img src= '/public/compass.png'
-            className='p-7'
-            />
+
+    return (
+        <div className='flex items-center w-full justify-between border-b 1px'>
+            <div className='w-[90px] h-[90px] bg-black '>
+                <img src='/public/compass.png'
+                    className='p-7'
+                />
+            </div>
+            <div className='flex gap-14'>
+                {menu.map((item) => (
+                    // eslint-disable-next-line react/jsx-key
+                    <div className='cursor-pointer hover:underline font-medium'>
+                        {item.name}
+                    </div>
+                ))}
+            </div>
+            <div className='w-[90px] h-[90px] bg-red-500 flex justify-center items-center'
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10 text-white">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+                </svg>
+
+            </div>
         </div>
-        <div>
-            {menu.map((item)=> (
-                // eslint-disable-next-line react/jsx-key
-                <div className='cursor-pointer'>
-                  {item.name}
-                </div>
-            ))}
-        </div>
-    </div>
-  )
+    )
 };
 
 export default Header
